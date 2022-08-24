9e4043fbb9474d38b88e0a1b095e7f382a5305f1
AndraRice <111477652+AndraRice@users.noreply.github.com>
2022-08-24 16:08:24 -0600

    Add files via upload

---
+++ Lesson00.py
@@ -0,0 +1,37 @@
+# Lesson00, Andra Rice
+# Task:
+# Create a python script including:
+#   -A For loop
+#   -A While loop
+#   -A Use statement
+#   -Accepting user input
+#   -Returns output
+
+print("Enter an integer betweeen 1 and 100:")
+
+x=input() # read input from user
+
+x=int(x) # typecast x so we can make sure it's in the bounds!
+
+if x<1 or x>100:
+    print("The input entered does not meet the conditions.")
+
+    while x<1 or x>100: # continue to ask for new input until a reasonable int is entered
+        print("Enter an integer betweeen 1 and 100:")
+        x=input()
+        x=int(x) # typecast x
+
+range=range(1,x+1)
+i=1
+
+#print a number triangle 
+for i in range:
+    j=1
+    while j<=i:
+        print(i, end='')
+        print(" ", end='')
+        j=j+1
+    print("")
+
+
+
